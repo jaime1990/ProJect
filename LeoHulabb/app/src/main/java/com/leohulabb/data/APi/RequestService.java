c5b0e8c4da462483e80f0b538116905df3089d74
Leo <703473298@qq.com>
2016-10-13 13:10:21 +0800

    no message

--- LeoHulabb/app/src/main/java/com/leohulabb/data/APi/RequestService.java
+++ LeoHulabb/app/src/main/java/com/leohulabb/data/APi/RequestService.java
@@ -16,11 +16,7 @@ import rx.Observable;
  * API接口
  * 因为使用RxCache作为缓存策略 所以这里不需要写缓存信息
  */
-public interface MovieService {
-
-    //获取视频地址
-    @GET("classroom/getHotVideos")
-    Observable<HttpResult<List<VideoListDto>>> getVideoList(@Query("pageIndex") int pageIndex, @Query("pageSize") int pageSize);
+public interface RequestService {
 
     //获取学校列表
     @POST("Colleges/getCollegeList")
--- LeoHulabb/app/src/main/java/com/leohulabb/data/HttpData/HttpData.java
+++ LeoHulabb/app/src/main/java/com/leohulabb/data/HttpData/HttpData.java
@@ -1,17 +1,15 @@
 package com.leohulabb.data.HttpData;
 
-import com.leohulabb.data.APi.MovieService;
+import com.leohulabb.data.APi.RequestService;
 import com.leohulabb.data.APi.SchoolBody;
 import com.leohulabb.data.HttpResult;
 import com.leohulabb.data.Retrofit.ApiException;
 import com.leohulabb.data.Retrofit.RetrofitUtils;
 import com.leohulabb.data.UniversityListDto;
-import com.leohulabb.data.VideoListDto;
 
 import java.util.List;
 
 import rx.Observable;
-import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
@@ -20,18 +18,12 @@ import rx.schedulers.Schedulers;
  *所有的请求数据的方法集中地
  * 根据MovieService的定义编写合适的方法
  */
-public class HttpData extends RetrofitUtils {
-
-//    private static File cacheDirectory = FileUtils.getcacheDirectory(BaseApplication.getInstance());
-//    private static final CacheProviders providers = new RxCache.Builder()
-//            .persistence(cacheDirectory)
-//            .using(CacheProviders.class);
-
-    protected static final MovieService service = getRetrofit().create(MovieService.class);
-
+public class HttpData extends RetrofitUtils
+{
+    private static final RequestService service = getRetrofit().create(RequestService.class);
 
     //在访问HttpMethods时创建单例
-    private static class SingletonHolder{
+    private static class SingletonHolder {
         private static final HttpData INSTANCE = new HttpData();
     }
 
@@ -40,41 +32,19 @@ public class HttpData extends RetrofitUtils {
         return SingletonHolder.INSTANCE;
     }
 
-    //Get请求  视频列表
-    public void verfacationCodeGetCache(int pageIndex, int pageSize, Observer<List<VideoListDto>> observer) {
-        Observable observable=service.getVideoList(pageIndex, pageSize).map(new HttpResultFunc<List<VideoListDto>>());
-//        Observable observableCahce=providers.getVideoList(observable,new DynamicKey("视频列表"+pageIndex+pageSize),new EvictDynamicKey(false)).map(new HttpResultFuncCcche<List<VideoListDto>>());
-//        setSubscribe(observableCahce,observer);
-    }
-
+    /**
+     * 请求学校列表
+     * @param pageIndex 页序号
+     * @param pageSize  页大小
+     * @return 事件源Observable
+     */
     public Observable<List<UniversityListDto>> HttpDataToSchoolList(int pageIndex, int pageSize) {
         return service.getSchoolList(new SchoolBody("", "", "", "", pageIndex, pageSize))
                 .map(new HttpResultFunc<List<UniversityListDto>>())
                 .distinct()
                 .subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
-
-    }
-
-    //post请求 学校列表
-    public void HttpDataToSchoolList(int pageIndex, int pageSize,Observer<List<UniversityListDto>> observer){
-        Observable observable=service.getSchoolList(new SchoolBody("","","","",pageIndex,pageSize))
-                .map(new HttpResultFunc<List<UniversityListDto>>());
-//        Observable observableCahce=providers.getVideoList(observable,new DynamicKey("学校列表"+pageIndex+pageSize),new EvictDynamicKey(false)).map(new HttpResultFuncCcche<List<UniversityListDto>>());
-//        setSubscribe(observableCahce,observer);
-    }
-
-    /**
-     * 插入观察者
-     * @param observable
-     * @param observer
-     * @param <T>
-     */
-    public static <T> void setSubscribe(Observable<T> observable, Observer<T> observer) {
-        observable.subscribeOn(Schedulers.io())
-                .subscribeOn(Schedulers.newThread())//子线程访问网络
-                .observeOn(AndroidSchedulers.mainThread())//回调到主线程
-                .subscribe(observer);
     }
 
     /**
@@ -82,18 +52,19 @@ public class HttpData extends RetrofitUtils {
      *
      * @param <T>   Subscriber真正需要的数据类型，也就是Data部分的数据类型
      */
-    private  class HttpResultFunc<T> implements Func1<HttpResult<T>, T> {
+    private class HttpResultFunc<T> implements Func1<HttpResult<T>, T> {
 
         @Override
         public T call(HttpResult<T> httpResult) {
 
-                if (httpResult.getCode() !=1 ) {
+                if (httpResult.getCode() != 1) {
                     throw new ApiException(httpResult);
                 }
 
             return httpResult.getResults();
         }
     }
+
     /**
      * 用来统一处理RxCacha的结果
      */
